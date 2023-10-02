2fe293addd5128f6880dde1399c0ec69e256f52b
比蹦 <SLMSkret1o146854@gmail.com>
2023-10-03 06:40:30 +0800

    Add Utils.java

---
+++ src/main/java/bs/untitled10/Utils/Utils.java
@@ -0,0 +1,44 @@
+package bs.untitled10.Utils;
+
+import bs.untitled10.Main;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.block.Block;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.metadata.FixedMetadataValue;
+import org.bukkit.metadata.MetadataValue;
+
+import java.util.List;
+
+public class Utils {
+
+    public static NamespacedKey createNamespacedKey(String key) {
+        return new NamespacedKey(Main.instance, key);
+    }
+
+    public static ItemStack createItemStack(Material material) {
+        return new ItemStack(material);
+    }
+
+    public static ItemStack setDisplayNameOfItemStack(ItemStack itemStack, String displayName) {
+        ItemMeta itemMeta = itemStack.getItemMeta();
+        if (itemMeta == null) return itemStack;
+
+        itemMeta.setDisplayName(displayName);
+        itemStack.setItemMeta(itemMeta);
+        return itemStack;
+    }
+
+
+    public static void setMetadataTag(Block block) {
+        if (hasMetadataTag(block)) return;
+        block.setMetadata("untitled10", new FixedMetadataValue(Main.instance, "canbemined"));
+    }
+
+    public static boolean hasMetadataTag(Block block) {
+        List<MetadataValue> metadataList = block.getMetadata("untitled10");
+        return !metadataList.isEmpty();
+    }
+
+}
